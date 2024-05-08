ea5cd02d4971ce9a6202e178a97452470d22033e
Imran Ahmad <ahmadimrankh99@gmail.com>
2024-05-08 23:45:20 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+let country, continent, population;
+let isisland = "No";
+country = "Pakistan";
+continent = "Asia";
+population = "241.5";
+console.log(country,continent,population);
+console.log("Not an Island", isisland);
+
+// Lecture 3
+
+const language = "Urdu";
+
+// Lecture 4
+  let halfpop = 241/2;
+  let popul = halfpop/2;
+  console.log("People :",popul);
+  console.log("Increased Population :",popul+1);
+  
+
